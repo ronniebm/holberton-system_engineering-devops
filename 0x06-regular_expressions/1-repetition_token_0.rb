cba6a3d720f95e04d386cb1ec1ed4cd735f9c833
ronniebm <ronnie.coding@gmail.com>
2020-07-06 11:04:14 -0500

    using holb format task#1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -3,5 +3,4 @@
 # regular expression that will match the cases:
 # MATCH CASES: hbttn, hbtttn, hbttttn, hbtttttn.
 # IGNORED CASES EXAMPLES: hbn, hbtn, hbttttttn.
-string = ARGV[0]
-string.scan(/hbt{2,5}n/) { |match| print "#{match}" }
+puts ARGV[0].scan(/hbt{2,5}n/).join
