9f58b9de0ca1861befff933e6aeafb63c9871818
ronniebm <ronnie.coding@gmail.com>
2020-07-06 09:09:57 -0500

    Uploading Task#1

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+# using string scan method to find words into a string.
+# http s://ruby-doc.org/core-2.5.1/String.html#method-i-scan
+string = ARGV[0]
+string.scan(/hbt{2,5}n/) { |match| print "#{match}" }
