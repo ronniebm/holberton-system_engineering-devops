09bdfafc932921945543da6e73e7debe9dfbbee2
ronniebm <ronnie.coding@gmail.com>
2020-07-06 09:32:39 -0500

    adding ignored cases examples to comments

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 # regular expression that will match the cases:
-# hbttn, hbtttn, hbttttn, hbtttttn.
+# MATCH CASES: hbttn, hbtttn, hbttttn, hbtttttn.
+# IGNORED CASES EXAMPLES: hbn, hbtn, hbttttttn.
 string = ARGV[0]
 string.scan(/hbt{2,5}n/) { |match| print "#{match}" }
