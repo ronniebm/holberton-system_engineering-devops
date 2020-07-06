2e469d2f172e99a6cf977a46e1739fa872ff0849
ronniebm <ronnie.coding@gmail.com>
2020-07-06 11:05:17 -0500

    using holb format task#6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -6,7 +6,4 @@
 # IGNORED CASE#1: ./6-phone_number.rb " 4155049898" | cat -e
 # IGNORED CASE#2: ./6-phone_number.rb "415 504 9898" | cat -e
 # IGNORED CASE#3: ./6-phone_number.rb "415-504-9898" | cat -e
-
-string = ARGV[0]
-# Ensure the number consists entirely of seven digits, nothing else.
-string.scan(/\A\d{10}/) { |match| print "#{match}" }
+puts ARGV[0].scan(/^\d{10}$/).join
