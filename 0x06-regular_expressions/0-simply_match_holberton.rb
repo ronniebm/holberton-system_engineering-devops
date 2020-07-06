3127ff1b92ef4dd1c09c58a1270563e9744747ee
ronniebm <ronnie.coding@gmail.com>
2020-07-06 11:04:01 -0500

    using holb format task#0

--- 0x06-regular_expressions/0-simply_match_holberton.rb
+++ 0x06-regular_expressions/0-simply_match_holberton.rb
@@ -2,5 +2,4 @@
 
 # using string scan method to find words into a string.
 # http s://ruby-doc.org/core-2.5.1/String.html#method-i-scan
-string = ARGV[0]
-string.scan(/Holberton/) { |match| print "#{match}" }
+puts ARGV[0].scan(/Holberton/).join
