55ae0b5113bbf5080f1badae02d731157a49e5fc
ronniebm <ronnie.coding@gmail.com>
2020-08-03 22:47:46 +0000

    fixing file directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # create a file containing 'I love Puppet'.
 file { '/tmp/holberton':
-ensure  => 'present',
-content => 'I love Puppet',
+ensure  => present,
+path    => '/tmp/holberton',
 mode    => '0744',
+owner   => 'www-data',
 group   => 'www-data',
-owner   => 'www-data'
+content => 'I love Puppet'
 }
