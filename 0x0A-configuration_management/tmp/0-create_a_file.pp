fcd7e488999beb0f7afd5e920aff346e097cb107
ronniebm <ronnie.coding@gmail.com>
2020-08-03 22:38:41 +0000

    adding 1rst line comment

--- 0x0A-configuration_management/tmp/0-create_a_file.pp
+++ 0x0A-configuration_management/tmp/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# create a file containing 'I love Puppet'.
 file { '/tmp/holberton':
-    ensure  => 'present',
-    content => 'I love Puppet',
-    mode    => '0744',
-    group   => 'www-data',
-    owner   => 'www-data'
+ensure  => 'present',
+content => 'I love Puppet',
+mode    => '0744',
+group   => 'www-data',
+owner   => 'www-data'
 }
