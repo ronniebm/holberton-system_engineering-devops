c229ea0e6c73e6c4d420dd9bb296303ec954a46d
ronniebm <ronnie.coding@gmail.com>
2020-08-14 16:38:15 -0500

    try#04

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,19 +1,15 @@
 # customizing HTTP header with Puppet.
-exec { 'update':
-  command  => 'apt-get update',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
--> package { 'nginx':
-  ensure  => present,
+-> package {'nginx':
+  ensure => 'present',
 }
-
--> file_line { 'header_customization':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => ':80 default_server;',
-  line   => "add_header X-Served-By ${hostname};",
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
--> exec { 'nginx_run':
-  command => 'service nginx start',
+-> exec {'run2':
+  command => '/usr/sbin/service nginx start',
 }
