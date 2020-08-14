c4591c3ce778cfb4e328b274763d297a4feb27c6
ronniebm <ronnie.coding@gmail.com>
2020-08-14 16:26:26 -0500

    try#03

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,17 +3,17 @@ exec { 'update':
   command  => 'sudo apt-get update',
 }
 
-package { 'nginx':
+-> package { 'nginx':
   ensure  => present,
 }
 
-file_line { 'header_customization':
+-> file_line { 'header_customization':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => ':80 default_server;',
   line   => "add_header X-Served-By ${hostname};",
 }
 
-exec { 'nginx_restart':
+-> exec { 'nginx_restart':
   command => 'sudo service nginx restart',
 }
