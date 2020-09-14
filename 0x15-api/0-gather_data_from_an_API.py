91a2a25de61e90e17b9232669a5e8f38a46f23c5
ronniebm <ronnie.coding@gmail.com>
2020-09-14 11:19:04 -0500

    first commit task#0, 100% tested

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""2-read_lines.py module"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+API_URL = "https://jsonplaceholder.typicode.com/"
+people = requests.get(API_URL + "users/" + argv[1])
+list_tasks = requests.get(API_URL + "todos?userId=" + argv[1])
+name = people.json().get("name")
+tasks = len(list_tasks.json())
+done = 0
+
+
+for task in list_tasks.json():
+    if task.get("completed"):
+        done += 1
+
+print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
+
+for task in list_tasks.json():
+    if task.get("completed"):
+        print("\t", end="")
+        print(task.get("title"))
