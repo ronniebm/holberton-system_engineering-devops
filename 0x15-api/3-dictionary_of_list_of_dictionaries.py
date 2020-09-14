15a0c160ff0012bd1675fc80ea61fea3c80cc0ee
ronniebm <ronnie.coding@gmail.com>
2020-09-14 15:53:53 -0500

    fixing pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,25 +5,24 @@ if __name__ == "__main__":
     import requests
     import json
 
-API_URL = "https://jsonplaceholder.typicode.com/"
-counter = len(requests.get(API_URL + "users").json())
-data = {}
-full_data = {}
+    API_URL = "https://jsonplaceholder.typicode.com/"
+    counter = len(requests.get(API_URL + "users").json())
+    data = {}
+    full_data = {}
 
-for i in range(0, counter):
+    for i in range(0, counter):
+        user = requests.get(API_URL + "users/" + str(i+1))
+        list_tasks = requests.get(API_URL + "todos?userId=" + str(i+1))
+        user_name = user.json().get("username")
+        l_dict = []
 
-    user = requests.get(API_URL + "users/" + str(i+1))
-    list_tasks = requests.get(API_URL + "todos?userId=" + str(i+1))
-    user_name = user.json().get("username")
-    l_dict = []
+        for task in list_tasks.json():
+            l_dict.append(dict(task=task.get("title"),
+                               completed=task.get("completed"),
+                               username=user_name))
 
-    for task in list_tasks.json():
-        l_dict.append(dict(task=task.get("title"),
-                           completed=task.get("completed"),
-                           username=user_name))
+        data = {(i+1): l_dict}
+        full_data.update(data)
 
-    data = {(i+1): l_dict}
-    full_data.update(data)
-
-with open('todo_all_employees.json', 'w') as file:
-    json.dump(full_data, file)
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(full_data, file)
