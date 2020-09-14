23eec31d4cd73784abeae2ff1b8dabfc1b15bc9e
ronniebm <ronnie.coding@gmail.com>
2020-09-14 15:42:34 -0500

    fixing indentation main=name

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,22 +6,22 @@ if __name__ == "__main__":
     import csv
     from sys import argv
 
-API_URL = "https://jsonplaceholder.typicode.com/"
-user = requests.get(API_URL + "users/" + argv[1])
-list_tasks = requests.get(API_URL + "todos?userId=" + argv[1])
-user_name = user.json().get("username")
+    API_URL = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(API_URL + "users/" + argv[1])
+    list_tasks = requests.get(API_URL + "todos?userId=" + argv[1])
+    user_name = user.json().get("username")
 
 
-with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
-    fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
+        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-    writer.writeheader()
+        writer.writeheader()
 
-    for task in list_tasks.json():
-        to_do = task.get("title")
-        state = str(task.get("completed"))
-        writer.writerow({'USER_ID': argv[1],
-                         'USERNAME': user_name,
-                         'TASK_COMPLETED_STATUS': state,
-                         'TASK_TITLE': to_do})
+        for task in list_tasks.json():
+            to_do = task.get("title")
+            state = str(task.get("completed"))
+            writer.writerow({'USER_ID': argv[1],
+                             'USERNAME': user_name,
+                             'TASK_COMPLETED_STATUS': state,
+                             'TASK_TITLE': to_do})
