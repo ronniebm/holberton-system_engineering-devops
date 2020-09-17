b26918b5cb1a00c6ecd05c06b79dfa5e4156335c
ronniebm <ronnie.coding@gmail.com>
2020-09-17 17:57:44 -0500

    fixing

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-1-top_ten
+2-recurse
 """
 import requests
 
@@ -12,15 +12,20 @@ def recurse(subreddit, hot_list=[], after=""):
     url = "https://api.reddit.com/r/{}/hot".format(subreddit)
     headers = {'User-Agent': 'ronnie'}
     arg1 = {"limit": 100, "after": after}
-    resp = requests.get(url, params=arg1, headers=headers).json()
-    list_a = resp.get('data', {}).get('children', None)
-    pagination = resp.get('data', {}).get('after', None)
-
-    if list_a:
-        for item in list_a:
-            hot_list.append(item.get("data").get("title"))
+    resp = requests.get(url, params=arg1, headers=headers)
+    list_a = resp.json().get('data', {}).get('children', None)
+    pagination = resp.json().get('data', {}).get('after', None)
 
     if pagination is not None:
-        recurse(subreddit, hot_list, pagination)
 
-    return hot_list
+        if list_a:
+            for item in list_a:
+                hot_list.append(item.get("data").get("title"))
+
+        if pagination is not None:
+            recurse(subreddit, hot_list, pagination)
+
+        return hot_list
+
+    else:
+        return None
